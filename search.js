e61ab6c1acd406e66799b6f15c4487bc84ec6f5d
brunopojani <brunopojani@gmail.com>
2018-01-26 10:56:21 +0100

    format display
    format the code display

--- search.js
+++ search.js
@@ -1,9 +1,12 @@
  function myFunction() {
+  
   var input, filter, table, tr, td, i;
+  
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
+  
   for (i = 0; i < tr.length; i++) {
     td1 = tr[i].getElementsByTagName("td")[0];
     td2=tr[i].getElementsByTagName("td")[1];
